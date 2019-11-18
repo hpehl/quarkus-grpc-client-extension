6ee43d6f460fe3309a9abe78b4271374e43a11ce
Harald Pehl <harald.pehl@gmail.com>
2019-11-18 15:33:31 +0100

    Change configuration root phase to RUN_TIME

--- runtime/src/main/java/io/quarkus/grpc/client/runtime/GrpcClientConfig.java
+++ runtime/src/main/java/io/quarkus/grpc/client/runtime/GrpcClientConfig.java
@@ -1,14 +1,14 @@
 package io.quarkus.grpc.client.runtime;
 
-import static io.quarkus.runtime.annotations.ConfigPhase.BUILD_AND_RUN_TIME_FIXED;
-
 import java.util.Map;
 
 import io.quarkus.runtime.annotations.ConfigItem;
 import io.quarkus.runtime.annotations.ConfigRoot;
 
+import static io.quarkus.runtime.annotations.ConfigPhase.RUN_TIME;
+
 /** gRPC client configuration. */
-@ConfigRoot(phase = BUILD_AND_RUN_TIME_FIXED)
+@ConfigRoot(phase = RUN_TIME)
 public class GrpcClientConfig {
 
     /** Channel configuration */
